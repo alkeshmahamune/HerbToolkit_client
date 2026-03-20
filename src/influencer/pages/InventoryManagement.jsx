9a5b321c7275f94403263f5668fdaedbbc3d4c2e
alkeshmahamune <alkeshmahamune@gmail.com>
2026-03-20 15:47:58 +0530

    added inventory in influencer

---
+++ src/influencer/pages/InventoryManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InventoryManager from '../../components/GroceryAndInventory'
+
+const InventoryManagement = () => {
+  return (
+    <div>
+      <InventoryManager/>
+    </div>
+  )
+}
+
+export default InventoryManagement
